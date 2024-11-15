ec0ef8473b1aaedc1081135977d23472dff34d6a
IlianaM1 <iliana.madrigal.839@my.csun.edu>
2024-11-15 11:09:32 -0800

    Create ManageReservation.java
    The ManageReservation class allows for the user to be able to edit their reservation and it be saved into the text file or to cancel the reservation and have it get deleted from the text file

---
+++ ManageReservation.java
@@ -0,0 +1,69 @@
+import java.io.*;
+import java.util.*;
+
+public class ManageReservation {
+
+    public void changeReservation(String ID, String oldValue, String newValue){
+    try{
+        BufferedReader reader = new BufferedReader(new FileReader("Reservations.txt"));
+        BufferedWriter writer = new BufferedWriter((new FileWriter("temp.txt")));
+        String line;
+        while((line = reader.readLine()) != null){
+            if(line.equals(ID)){
+                writer.write(line);
+                writer.newLine();
+                for(int i =0; i< 6; i++) {
+                    line= reader.readLine();
+                    line = line.replace(oldValue, newValue);
+                    writer.write(line);
+                    writer.newLine();
+                }
+            }else{
+                writer.write(line);
+                writer.newLine();
+            }
+        }
+        writer.close();
+        reader.close();
+
+    }catch (Exception e){System.out.printf("\n\tError occurred in trying to write to the file. %s", e);}
+    try{
+        File oldName = new File("Reservations.txt");
+        boolean test = oldName.delete();
+        File temp = new File("temp.txt");
+        File newName = new File("Reservations.txt");
+        boolean check = temp.renameTo(newName);
+    }catch (Exception e){System.out.printf("\n\tError occurred in trying to rename and delete files. %s", e);}
+
+    }
+
+    public void cancelRes(String ID){
+        try{
+            BufferedReader reader = new BufferedReader(new FileReader("Reservations.txt"));
+            BufferedWriter writer = new BufferedWriter((new FileWriter("temp.txt")));
+            String line;
+            while((line = reader.readLine()) != null){
+                if(line.equals(ID)){
+                    for(int i = 0; i < 6; i++){
+                        line = reader.readLine();
+                    }
+                }else{
+                    writer.write(line);
+                    writer.newLine();;
+                }
+            }
+            reader.close();
+            writer.close();
+
+        }catch (Exception e){System.out.printf("\n\tError occurred in trying to write to the file. %s", e);}
+        try{
+            File oldName = new File("Reservations.txt");
+            boolean test = oldName.delete();
+            File temp = new File("temp.txt");
+            File newName = new File("Reservations.txt");
+            boolean check = temp.renameTo(newName);
+        }catch (Exception e){System.out.printf("\n\tError occurred in trying to rename and delete files. %s", e);}
+
+    }
+
+}
