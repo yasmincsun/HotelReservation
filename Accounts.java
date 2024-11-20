f71b873a08e47741672ca08c12a57d67809cc2e2
yasmincsun <yasmin.zubair.180@my.csun.edu>
2024-11-20 15:00:15 -0800

    Update Accounts.java
    Added 2/4 of the Javadoc style comments and the framework for the other 2/4 Javadoc style comments

--- Accounts.java
+++ Accounts.java
@@ -3,8 +3,20 @@ package org.example.hotelreservations;
 import java.util.*;
 import java.io.*;
 
+/**
+* DESCRIPT OF CLASS
+* @author Iliana Madirgal 
+* @version 1.2 
+*/ 
+
 public class Accounts {
 
+    /**
+    *DESCRIPT
+    * @author Iliana Madrigal 
+    * @param num is the length of the AccountCode. 
+    * @return code passes the AccountID as a String back to the method that called AccountCode(). 
+    */ 
     public String AccountCode(int num) {
         String NumbString = "0123456789";
         StringBuilder code = new StringBuilder(num);
@@ -14,7 +26,17 @@ public class Accounts {
         }
         return code.toString();
     }
-
+    
+    /**
+    * The verifyAccount method is used to ensure that the user who is logging into the program is the one who is meant to. It calls the AccountSearch method to verify that the 
+    *     accountID and the last name associated with the account are the same. Returns true if the account information is verified and false if it is not. 
+    * @author Iliana Madrigal 
+    * @param accountID is the unique identifier that differentiates every account form one another. This is used to search through the Accounts.txt file when used as a parameter 
+    *     in the AccountSearch method.
+    * @param lname is the user's last name that they input into the program. The lname value should be the same as the one associated with the accountID to verify the account or not. 
+    * @return boolean value is returned to the class or method that calls verifyAccount() that is either true or false depending on whether or not the accountID and the lname
+    *     values that the user inputs into the system match what is in the Accounts.txt file. 
+    */ 
     public boolean verifyAccount(String accountID, String lname) {
         AccountSearch obj = new AccountSearch(accountID);
         String lastname = obj.getlname();
@@ -25,6 +47,17 @@ public class Accounts {
             return false;
         }
     }
+
+     /**
+    *The createAccount method creates the account with information inputted by the user, and writes the information into Accounts.txt. 
+    *    The createAccount method also calls the AccountCode method to generate the accountID which is used to differentiate all of the accounts from one another. 
+    * @author Iliana Madrigal 
+    * @param firsName is the user's first name that they input into the program.
+    * @param lastName is the user's last name that they input into the program
+    * @param phoneNumber is the user's phone number that they input into the program
+    * @param email is the user's email address that they input into the program. 
+    * @exception if the information is unable to be written to the Accounts.txt file for any reason, the exception catches the issue and prints the error to the terminal. 
+    */ 
 
     public void createAccount(String firstName, String lastName, String phoneNumber, String email) {
         try {
