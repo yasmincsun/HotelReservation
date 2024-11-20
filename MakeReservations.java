3469e598da47be87eb6f9d862728f496a468b480
yasmincsun <yasmin.zubair.180@my.csun.edu>
2024-11-20 07:39:46 -0800

    Update MakeReservations.java
    Added 2/3 of the necessary Javadoc style comments

--- MakeReservations.java
+++ MakeReservations.java
@@ -4,8 +4,20 @@ import java.io.*;
 //This code will gather all of the users information and information about the reservation
 //and create the reservation and put it into the Reservations.txt file
 
+/**
+* DESCRIPTION OF CLASS HERE 
+* @author Iliana Madrigal 
+* @version 1.3 
+*/ 
+
 public class MakeReservations {
 
+    /**
+    * Creates a Reservaton Confirmation Code after a reservation is created by the user to differentiate between different bookings. 
+    * @author Iliana Madrigal 
+    * @param num serves as the length of the Reservation Confirmation code and is declared in the makeRes method's code that calls the ConfirmCode method
+    * @return The confirmation code as a String type 
+    */ 
     public static String ConfirmCode(int num){
         String AlphabetString = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
         StringBuilder code = new StringBuilder(num);
@@ -16,7 +28,16 @@ public class MakeReservations {
         return code.toString();
     }
   
-
+    /**
+    * Creates the reservation and writes the information into Reservations.txt in the order in which is it passed into the method, preceded by the code which is created by calling ConfirmCode(num)
+    * @author Iliana Madrigal 
+    * @param firstName is the user's first name that they input while creating a reservation
+    * @param lastName is the user's last name that they input while creating a reservation 
+    * @param checkInDate is the date that the user will check into the hotel
+    * @param checkOutDate is the date that the user will checkout of the hotel 
+    * @param roomType is the bed type that the user selected when making a reservation. User had a choice between a King Bed Room and a Two Queen Bed Room
+    * @exception if the information is unable to be written into the file for any reason, the exception is thrown and an error message is printed to the terminal
+    */ 
     public void makeRes(String firstName, String lastName) {
         try{
             int num = 6;
