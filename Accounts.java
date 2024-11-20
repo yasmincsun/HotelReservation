b12d9b64312dd74ced68551019000f46ce8fdcee
yasmincsun <yasmin.zubair.180@my.csun.edu>
2024-11-20 08:44:32 -0800

    Update Accounts.java
    Updated Accounts.java with the working code from IntelliJ

--- Accounts.java
+++ Accounts.java
@@ -1,53 +1,64 @@
+package org.example.hotelreservations;
+
 import java.util.*;
 import java.io.*;
 
 public class Accounts {
 
-    public static String AccountCode(int num){
+    public String AccountCode(int num) {
         String NumbString = "0123456789";
         StringBuilder code = new StringBuilder(num);
-        for(int i = 0; i < num; i++){
-            int index = (int) (NumbString.length()* Math.random());
+        for (int i = 0; i < num; i++) {
+            int index = (int) (NumbString.length() * Math.random());
             code.append(NumbString.charAt(index));
         }
         return code.toString();
     }
-    public boolean verifyAccount(String accountID, String lname){
+
+    public boolean verifyAccount(String accountID, String lname) {
         AccountSearch obj = new AccountSearch(accountID);
         String lastname = obj.getlname();
         boolean result = obj.getResult();
-        if(result == true && lname.equals(lastname)){
+        if (result == true && lname.equals(lastname)) {
             return true;
-        }else{
+        } else {
             return false;
         }
     }
 
+    public void createAccount(String firstName, String lastName, String phoneNumber, String email) {
+        try {
+            int num = 6;
 
-public static void main(String[] args) {
-    try{
-        int num = 6;
+            String fname = firstName;
+            String lname = lastName;
+            String phone = phoneNumber;
+            String emailAddress = email;
+
+            String code = AccountCode(num);
+
+            BufferedWriter writer = new BufferedWriter(new FileWriter("Accounts.txt", true));
 
 
-        String fname = "John";
-        String lname = "Smith";
+            writer.write(code);
+            writer.newLine();
+            writer.write(fname);
+            writer.newLine();
+            writer.write(lname);
+            writer.newLine();
+            writer.write(phone);
+            writer.newLine();
+            writer.write(emailAddress);
+            writer.newLine();
 
-        String code = AccountCode(num);
+            writer.newLine();
 
-        BufferedWriter writer = new BufferedWriter(new FileWriter("accounts.txt",true));
+            writer.close();
 
 
-        writer.write(code);
-        writer.newLine();
-        writer.write(fname);
-        writer.newLine();
-        writer.write(lname);
-        writer.newLine();
-        writer.newLine();
-
-        writer.close();
-
-    }catch (Exception e){System.out.printf("\n\tError occurred in trying to write to the file. %s", e);}
+        } catch (Exception e) {
+            System.out.printf("\n\tError occurred in trying to write to the file. %s", e);
+        }
     }
 
 }
