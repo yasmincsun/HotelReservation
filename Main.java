bde467df422cedb2d2b94fb0dfaf79daad531065
yasmincsun <yasmin.zubair.180@my.csun.edu>
2024-11-22 10:40:34 -0800

    Finished Javadoc Comments

--- Main.java
+++ Main.java
@@ -1,6 +1,5 @@
 package org.example.hotelreservations;
 
-
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
@@ -9,9 +8,21 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 
-
+/**
+* This class simply sets the Stage for the JavaFX files to be displayed. All of the scenes are loaded onto this stage for the entire program. 
+* @author Yasmin Zubair 
+* @version 1.4
+*/ 
 public class Main extends Application {
     @Override
+
+    /**
+    * The start method loads the HotelMain-View.fxml file to the system and displays the scene to the user through the stage. The stage title is also set, which is displayed to the user
+    *     at the top of the stage. 
+    * @author Yasmin Zubair
+    * @param primaryStage is the necessary background in which all of the scenes need to be loaded through.
+    * @thorws IOException if there is any issues with loading the scene or receiving the input from the user, the exception is thrown to ensure that the program can continue to function. 
+    */ 
     public void start(Stage primaryStage) throws IOException {
         Parent root = FXMLLoader.load(getClass().getResource("HotelMain-View.fxml"));
         primaryStage.setTitle("Tree Hotel Reservation App");
